e27fd6f71d4f5d6b663a17564944f914da204edc
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-12-13 07:23:36 +0900

    [add] state and prop sample

--- React/study/src/client.js
+++ React/study/src/client.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./components/Layout";
 import InnerFunc from "./components/InnerFunc";
-import ImportComp from "./components/ImportComp"
+import ImportComp from "./components/ImportComp";
+import SetState from "./components/SetState";
 
 const app = document.getElementById("app");
 
 // ReactDOM.render(<Layout/>, app);
 // ReactDOM.render(<InnerFunc/>, app);
 ReactDOM.render(<ImportComp/>, app);
+// ReactDOM.render(<SetState/>,app);
 
--- React/study/src/components/ImportComp.js
+++ React/study/src/components/ImportComp.js
@@ -3,8 +3,20 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class ImportComp extends React.Component {
+  constructor() {
+    super();
+    this.state = { title: "Welcome"};
+  }
   render() {
-    let components = [<Header/>,<Footer/>];
+    setTimeout(
+      () => { this.setState({title: "Welcom Sample state!!"});},
+      2000
+    );
+    let components = [
+        <Header title={this.state.title}/>,
+        <Header title="Props Translation!! sample"/>,
+        <Footer/>
+    ];
     return(
         <div>
           {components}
